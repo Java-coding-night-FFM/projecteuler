8b202263e6c44fecf3022daac0c6b86bca0f14aa
Carsten Habicht <ch@chabicht.com>
2014-10-17 20:54:40 +0200

    Problem 3.

---
+++ src/projecteuler/problem3/Problem3.java
@@ -0,0 +1,44 @@
+package projecteuler.problem3;
+
+public class Problem3 {
+	public static void main(String[] args) {
+		System.out
+				.println(new Problem3().findBiggestPrimeFactor(600851475143L));
+	}
+
+	public long findBiggestPrimeFactor(long number) {
+		long prime = 2;
+
+		if (number <= 2) {
+			return number;
+		}
+
+		while (number > prime) {
+			if (number % prime == 0)
+				number /= prime;
+			else
+				prime = findNextPrime(prime);
+		}
+
+		return prime;
+	}
+
+	private long findNextPrime(long prime) {
+		long candidate = prime + 1;
+
+		while (!isPrime(candidate)) {
+			candidate++;
+		}
+
+		return candidate;
+	}
+
+	private boolean isPrime(long candidate) {
+		for (long i = 2; i < candidate; i++) {
+			if (candidate % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
